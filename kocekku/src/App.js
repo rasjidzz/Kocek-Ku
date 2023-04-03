995d9f75c92fe3734451da773065d18b8ea78dcc
MUHAMMAD ZAKY FATHURAHIM <mzakyf5503@gmail.com>
2023-04-04 00:25:53 +0700

    init

---
+++ kocekku/src/App.js
@@ -0,0 +1,116 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex flex-row">
+      <div className="basis-[15%] align-middle border-r-[1.5px] border-[#DFE8F6]">
+        <div className="flex h-[68px]">
+          <div className="my-auto mx-auto">logo</div>
+        </div>
+        <div className="grid grid-cols mt-8 gap-5 ml-4">
+          <div className=" flex flex-row w-[20vh] py-4 px-4 rounded-md bg-[#6245D5] text-white">
+            <div className="basis-5 pr-3">logo</div>
+            <div className="">DashBoard</div>
+          </div>
+          <div className=" text-[#A69FAD] flex flex-row w-[20vh] py-4 px-4 rounded-md hover:bg-[#6245D5] hover:text-white">
+            <div className="basis-5 pr-3">logo</div>
+            <div className="">DashBoard</div>
+          </div>
+          <div className=" text-[#A69FAD] flex flex-row w-[20vh] py-4 px-4 rounded-md hover:bg-[#6245D5] hover:text-white">
+            <div className="basis-5 pr-3">logo</div>
+            <div className="">DashBoard</div>
+          </div>
+          <div className=" text-[#A69FAD] flex flex-row w-[20vh] py-4 px-4 rounded-md hover:bg-[#6245D5] hover:text-white">
+            <div className="basis-5 pr-3">logo</div>
+            <div className="">DashBoard</div>
+          </div>
+          <div className=" text-[#A69FAD] flex flex-row w-[20vh] py-4 px-4 rounded-md hover:bg-[#6245D5] hover:text-white">
+            <div className="basis-5 pr-3">logo</div>
+            <div className="">DashBoard</div>
+          </div>
+          <div className=" text-[#A69FAD] flex flex-row w-[20vh] py-4 px-4 rounded-md hover:bg-[#6245D5] hover:text-white">
+            <div className="basis-5 pr-3">logo</div>
+            <div className="">DashBoard</div>
+          </div>
+        </div>
+        <div className="mt-40 w-[80%] mx-auto h-[13rem] bg-[#FC9269] hover:bg-[#ff8a5c] rounded-2xl"></div>
+      </div>
+      <div className="basis-[100%] bg-[#F8F8F8]">
+        <div className="flex-col flex">
+          <div className="w-full h-[68px] bg-white border-b-[1.5px] border-[#DFE8F6] flex flex-row justify-between">
+            <div className="border-[#DFE8F6] w-[300px] h-full border-r-[1.5px] flex basis-[20%]">
+              <div className="m-auto text-[#192252]">
+                Wednesdey, 29 des 2021
+              </div>
+            </div>
+            <div className="div my-auto ml-6 basis-[60%]">logo</div>
+            <div className="flex my-auto gap-5 mx-auto">
+              <div className="my-auto">logo</div>
+              <div className="my-auto">
+                logo
+                <img src="" alt="" />
+              </div>
+              <div className="flex flex-col text-sm">
+                <div>Zaky</div>
+                <div>@zaky</div>
+              </div>
+              <div className="my-auto">
+                <button button="">logo</button>
+              </div>
+            </div>
+          </div>
+          <div className="basis-[80%]">
+            <div className="flex flex-row justify-between">
+              <div className="basis-[75%]"></div>
+              <div className="basis-[25%] bg-white max-h-full py-10 px-5">
+                <div className="flex flex-row justify-between">
+                  <div className="ml-4">My Card</div>
+                  <div className="mr-4">change card</div>
+                </div>
+                <div className="h-48 w-[100%] mt-5 rounded-2xl mx-auto bg-[#FC9269]">
+                  <div className="w-[100%] p-6 text-white">
+                    <div>My Balance</div>
+                    <div>$64,200.00</div>
+                    <div className="mt-12">Muhammad Zaky</div>
+                    <div className="flex flex-row justify-between">
+                      <div className="">1302-2213-0670-2210</div>
+                      <div className="">Visa</div>
+                    </div>
+                  </div>
+                  <div className="flex mt-5 flex-row justify-between gap-7">
+                    <div className="">
+                      <div className="w-[74px] border-2  h-[74px] flex justify-center rounded-lg hover:drop-shadow-2xl bg-[#6245D5] border-[#6245D5] text-white">
+                        <div className="my-auto">logo</div>
+                      </div>
+                      <div className="text-center mt-2">Transfer</div>
+                    </div>
+                    <div className="">
+                      <div className="w-[74px] border-2 border-[#DFE8F6] h-[74px] flex justify-center rounded-lg hover:drop-shadow-2xl hover:bg-[#6245D5] hover:border-[#6245D5] hover:text-white">
+                        <div className="my-auto">logo</div>
+                      </div>
+                      <div className="text-center mt-2">Transfer</div>
+                    </div>
+                    <div className="">
+                      <div className="w-[74px] border-2 border-[#DFE8F6] h-[74px] flex justify-center rounded-lg hover:drop-shadow-2xl hover:bg-[#6245D5] hover:border-[#6245D5] hover:text-white">
+                        <div className="my-auto">logo</div>
+                      </div>
+                      <div className="text-center mt-2">Transfer</div>
+                    </div>
+                    <div className="">
+                      <div className="w-[74px] border-2 border-[#DFE8F6] h-[74px] flex justify-center rounded-lg hover:drop-shadow-2xl hover:bg-[#6245D5] hover:border-[#6245D5] hover:text-white">
+                        <div className="my-auto">logo</div>
+                      </div>
+                      <div className="text-center mt-2">Transfer</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
